255efc2dc5c43dfc8616201ce24258793057dac7
esmiraSar <esmirasar@yandex.ru>
2024-10-31 19:41:20 +0300

    parsing page product

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
 
+list_category = []
+href = []
 
-def parsing(url):
-    response = requests.get(url)
+
+def parsing_catalog():
+    response = requests.get('https://yacht-parts.ru/catalog/')
 
     parsing_product = BeautifulSoup(response.text, 'html.parser')
 
-    products = parsing_product.find_all('div', class_='section_item')
-    list_category = []
+    products = parsing_product.find_all('div', class_='catalog_section_list')
+
     for section_item in products:
         category_name = section_item.find('a', class_='thumb').img['title']
+
         subcategory = section_item.find_all('li', class_='sect')
+
         for i in subcategory:
             list_category.append(i.a.text)
+            href.append(i.a['href'])
 
-    return list_category
+    return href
 
 
-url = 'https://yacht-parts.ru/catalog/'
-print(parsing(url))
-# TODO распарсить все категории
+def parsing_card():
+    list_name = []
+    for i in href:
+        list_name.append(i)
+        url_name = 'https://yacht-parts.ru/' + i
+        response = requests.get(url_name)
+        parsing_product = BeautifulSoup(response.text, 'html.parser')
+        parsing_name_product = parsing_product.find_all('div', class_='item-title')
+        for j in parsing_name_product:
+            list_name.append(j.find('span').text)
+
+    return list_name
+
+
+parsing_catalog()
+print(parsing_card())
